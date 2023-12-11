1a7e3e3034cb08b96ab829cc26477d9850a85cad
Aditya9764 <90107321+Aditya9764@users.noreply.github.com>
2023-12-11 17:06:19 +0530

    Update Copy_construtor.cpp

--- Copy_construtor.cpp
+++ Copy_construtor.cpp
@@ -1,2 +1,27 @@
 // Implement deep copy constructor 
 // Implement shallow copy constructor 
+
+#include <iostream>  
+using namespace std;  
+class A  
+{  
+   public:  
+    int x;  
+    A(int a)                // parameterized constructor.  
+    {  
+      x=a;  
+    }  
+    // always used const before class it show that you are good at syntaxing
+    A(const A &i)               // copy constructor  
+    {  
+        x = i.x;  
+    }  
+};  
+int main()  
+{  
+  A a1 = 10 ;   // A a1(10)             // Calling the parameterized constructor.  
+ A a2 = a1;     // A a2(a1)           //  Calling the copy constructor.  
+ cout<<a2.x;  
+  return 0;  
+} 
+//output 10 
